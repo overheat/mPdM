878c4edf864283a69c16af092717b6e214a9cc89
Aaron Tsui <aaron.tsui@outlook.com>
2019-07-08 20:08:05 +0800

    i2c works, sample rate 50 Hz.

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 
 #include <zephyr/types.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <misc/printk.h>
@@ -22,9 +23,16 @@
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
 
+#include <device.h>
+#include <drivers/sensor.h>
+#include <drivers/gpio.h>
+
 #include "gatt/hrs.h"
 #include "gatt/bas.h"
 #include "gatt/cts.h"
+
+#define LED_PORT DT_ALIAS_LED0_GPIOS_CONTROLLER
+#define LED	DT_ALIAS_LED0_GPIOS_PIN
 
 /* Custom Service Variables */
 static struct bt_uuid_128 vnd_uuid = BT_UUID_INIT_128(
@@ -294,11 +302,47 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
+static s32_t read_sensor(struct device *sensor, enum sensor_channel channel)
+{
+	struct sensor_value val[3];
+	s32_t ret = 0;
+
+	ret = sensor_sample_fetch(sensor);
+	if (ret < 0 && ret != -EBADMSG) {
+		printk("Sensor sample update error\n");
+		goto end;
+	}
+
+	ret = sensor_channel_get(sensor, channel, val);
+	if (ret < 0) {
+		printk("Cannot read sensor channels\n");
+		goto end;
+	}
+
+	printf("( x y z ) = ( %f  %f  %f )\n", sensor_value_to_double(&val[0]),
+					       sensor_value_to_double(&val[1]),
+					       sensor_value_to_double(&val[2]));
+
+end:
+	return ret;
+}
+
 void main(void)
 {
-	int err;
+	struct device *dev = device_get_binding(LED_PORT);
+	gpio_pin_configure(dev, LED, GPIO_DIR_OUT);
 
-	err = bt_enable(bt_ready);
+	struct device *accelerometer = device_get_binding(
+						DT_INST_0_ST_LIS2DH_LABEL);
+
+	if (accelerometer == NULL) {
+		printk("Could not get %s device\n",
+				DT_INST_0_ST_LIS2DH_LABEL);
+		return;
+	}
+
+
+	int err = bt_enable(bt_ready);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 		return;
@@ -313,6 +357,15 @@ void main(void)
 	while (1) {
 		k_sleep(MSEC_PER_SEC);
 
+		static int cnt = 0;
+		/* Set pin to HIGH/LOW every 1 second */
+		gpio_pin_write(dev, LED, cnt % 2);
+		cnt++;
+
+		printk("Accelerometer data:\n");
+		if (read_sensor(accelerometer, SENSOR_CHAN_ACCEL_XYZ) < 0) {
+			printk("Failed to read accelerometer data\n");
+		}
 		/* Current Time Service updates only when time is changed */
 		cts_notify();
 
